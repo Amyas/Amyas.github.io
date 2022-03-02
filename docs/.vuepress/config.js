f1003faa8fb5e252e8c8b5a4d797f690f61d1edf
wangjianpeng <wangjianpeng@weimiao.cn>
2022-03-02 18:53:53 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  title: "Amyas ' Blog",
+  description: "技术沉淀",
+  theme: 'reco',
+  // https://vuepress-theme-reco.recoluan.com/views/1.x/configJs.html#%E7%A7%BB%E5%8A%A8%E7%AB%AF%E4%BC%98%E5%8C%96
+  // 在移动端，搜索框在获得焦点时会放大，并且在失去焦点后可以左右滚动，这可以通过设置元来优化。
+  head: [
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+  ],
+  themeConfig: {
+    type: 'blog',
+    author: 'Amyas',
+    subSidebar: 'auto',
+    lastUpdated: 'Last Updated',
+    nav: [
+      { text: "首页", link: "/" },
+      {
+        text: '数据结构与算法',
+        link: '/data-structure-and-algorithm/'
+      },
+    ],
+    sidebar: {
+      '/data-structure-and-algorithm/': [
+        {
+          title: '数据结构与算法',
+          collapsable: false,
+          children: [['', '前言'], 'linked-list', 'binary-tree']
+        }
+      ],
+    }
+  },
+};
---
+++ docs/data-structure-and-algorithm/binary-tree.md
@@ -0,0 +1,11 @@
+# 二叉树
+
+## test
+
+### test1
+
+### test2
+
+### test3
+
+## testz
