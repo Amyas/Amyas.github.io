a7234d107fd335ed9e8c0b592f2d9c8448feed69
wangjianpeng <wangjianpeng@weimiao.cn>
2022-04-13 16:18:54 +0800

    docs(iq): 目录结构变化
    ew实现

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,61 @@
-const path = require('path')
-const fs = require('fs')
+const path = require("path");
+const fs = require("fs");
 
-function getChildrens(basePath, subModule){
-  const mdModule = path.join(__dirname, '../', basePath, subModule)
-  const moduleDirs = fs.readdirSync(mdModule)
-  return moduleDirs.map(dirName=>`/${basePath}/${subModule}/${dirName}`)
+const docsPath = path.join(__dirname, "..");
+
+function findDirDirectory(dirPath, filterDirs = []) {
+  return fs
+    .readdirSync(dirPath, {
+      withFileTypes: true,
+    })
+    .filter((v) => {
+      return v.isDirectory() && filterDirs.every((dir) => dir !== v.name);
+    })
+    .map((v) => v.name);
 }
 
+function generateSidebar(list = []) {
+  const sidebar = {};
+
+  while (list.length) {
+    const sidebarName = list.shift();
+    const sidebarItem = generateSidebarItem(sidebarName);
+    sidebar[`/${sidebarName}/`] = sidebarItem;
+  }
+
+  return sidebar;
+}
+
+function generateSidebarItem(dirName) {
+  const dirPath = path.join(__dirname, "..", dirName);
+
+  const folderList = findDirDirectory(dirPath).sort((a, b) => {
+    const splitDot = (v) => Number(v.split("、")[0]);
+    const numberA = splitDot(a);
+    const numberB = splitDot(b);
+
+    return numberA - numberB;
+  });
+
+  const sidebarItem = folderList.reduce((total, subDirName) => {
+    const item = {
+      title: subDirName,
+      children: generateSidebarChildren(dirName, subDirName),
+    };
+    total.push(item);
+    return total;
+  }, []);
+
+  return sidebarItem;
+}
+
+function generateSidebarChildren(basePath, subModule) {
+  const mdModule = path.join(__dirname, "../", basePath, subModule);
+  const moduleDirs = fs.readdirSync(mdModule);
+  return moduleDirs.map((dirName) => `/${basePath}/${subModule}/${dirName}`);
+}
+
+const folderList = findDirDirectory(docsPath, [".vuepress"]);
 
 module.exports = {
   title: "Amyas ' Blog",
@@ -32,76 +81,105 @@ module.exports = {
         link: "/frontend-engineering/",
       },
       {
+        text: "设计模式",
+        link: "/design-mode/",
+      },
+      {
         text: "数据结构与算法",
         link: "/data-structure-and-algorithm/",
       },
     ],
-    sidebar: [
-      {
-        title: '一、HTML/CSS',
-        children: getChildrens('interview-questions', 'htmlcss')
-      },
-      {
-        title: '二、HTTP',
-        children: getChildrens('interview-questions', 'http')
-      },
-      {
-        title: '三、JavaScript',
-        children: getChildrens('interview-questions', 'js')
-      },
-      {
-        title: '四、ES6',
-        children: getChildrens('interview-questions', 'es6')
-      }
-    ]
-    // sidebar: {
-    //   "/interview-questions/": [
-    //     {
-    //       title: "总结",
-    //       collapsable: false,
-    //       sidebarDepth: 1,
-    //       children: [
-    //         "/interview-questions/htmlcss/1"
-    //       ]
-    //       // path: "/interview-questions/html/",
-    //       // children: [
-    //       //   "htmlcss",
-    //       //   "js",
-    //       //   "async",
-    //       //   "seecodesaywhy",
-    //       //   "writecode",
-    //       //   "mobile",
-    //       //   "vue",
-    //       //   "engineering",
-    //       //   "git",
-    //       //   "algorithm",
-    //       //   "typescript",
-    //       //   "react",
-    //       //   "http",
-    //       // ],
-    //     },
-    //   ],
-    //   "/data-structure-and-algorithm/": [
-    //     {
-    //       title: "数据结构与算法",
-    //       path: "/data-structure-and-algorithm/stack/",
-    //       children: [
-    //         "stack",
-    //         "queue",
-    //         "priority-queue",
-    //         "linked-list",
-    //         "doubly-linked-list",
-    //         "binary-tree",
-    //       ],
-    //     },
-    //   ],
-    //   "/frontend-engineering/": [
-    //     {
-    //       title: "前端工程化",
-    //       path: "/frontend-engineering/git/",
-    //       children: ["git"],
-    //     },
-    //   ],
+    sidebar: generateSidebar(folderList), //{
+    // "/frontend-engineering/": [
+    //   {
+    //     title: '一、HTML/CSS',
+    //     children: getChildrens('interview-questions', 'html&css')
+    //   },
+    // ],
+    // "/interview-questions/": [
+    //   {
+    //     title: '一、HTML/CSS',
+    //     children: getChildrens('interview-questions', 'html&css')
+    //   },
+    //   {
+    //     title: '二、HTTP',
+    //     children: getChildrens('interview-questions', 'http')
+    //   },
+    //   {
+    //     title: '三、JavaScript',
+    //     children: getChildrens('interview-questions', 'js')
+    //   },
+    //   {
+    //     title: '四、ES6',
+    //     children: getChildrens('interview-questions', 'es6')
+    //   },
+    //   {
+    //     title: '五、Async',
+    //     children: getChildrens('interview-questions', 'async')
+    //   },
+    //   {
+    //     title: '六、WriteCode',
+    //     children: getChildrens('interview-questions', 'write-code')
+    //   },
+    //   {
+    //     title: '七、Mobile',
+    //     children: getChildrens('interview-questions', 'mobile')
+    //   },
+    //   {
+    //     title: '八、Vue2',
+    //     children: getChildrens('interview-questions', 'vue2')
+    //   },
+    //   {
+    //     title: '九、Vue3',
+    //     children: getChildrens('interview-questions', 'vue3')
+    //   },
+    //   {
+    //     title: '十、Engineering',
+    //     children: getChildrens('interview-questions', 'engineering')
+    //   },
+    //   {
+    //     title: '十一、Git',
+    //     children: getChildrens('interview-questions', 'git')
+    //   },
+    //   {
+    //     title: '十二、Algorithm',
+    //     children: getChildrens('interview-questions', 'algorithm')
+    //   },
+    //   {
+    //     title: '十三、DesignMode',
+    //     children: getChildrens('interview-questions', 'design-mode')
+    //   },
+    //   {
+    //     title: '十四、TypeScript',
+    //     children: getChildrens('interview-questions', 'typescript')
+    //   },
+    //   {
+    //     title: '十五、react',
+    //     children: getChildrens('interview-questions', 'react')
+    //   }
+    // ]
+    // }
+    // "/data-structure-and-algorithm/": [
+    //   {
+    //     title: "数据结构与算法",
+    //     path: "/data-structure-and-algorithm/stack/",
+    //     children: [
+    //       "stack",
+    //       "queue",
+    //       "priority-queue",
+    //       "linked-list",
+    //       "doubly-linked-list",
+    //       "binary-tree",
+    //     ],
+    //   },
+    // ],
+    // "/frontend-engineering/": [
+    //   {
+    //     title: "前端工程化",
+    //     path: "/frontend-engineering/git/",
+    //     children: ["git"],
+    //   },
+    // ],
     // },
   },
 };
---
+++ docs/.vuepress/test.js
@@ -0,0 +1,22 @@
+const arr = [
+  "0、HTTP",
+  "10、Git",
+  "11、NodeJS",
+  "12、Algorithm",
+  "13、DesignMode",
+  "14、TypeScript",
+  "15、React",
+  "1、HTML&CSS",
+  "2、JavaScript",
+  "3、ES6",
+  "4、Async",
+  "5、WriteCode",
+  "6、Mobile",
+  "7、Vue2",
+  "8、Vue3",
+  "9、Engineering",
+];
+const arr1 = ["0、asd", "10、asd", "11、asd", "1、asd"];
+
+console.log(
+);
