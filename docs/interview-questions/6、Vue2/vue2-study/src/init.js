fd5a10213a7b1f18467067b119508dda9e9f3037
wangjianpeng <wangjianpeng@weimiao.cn>
2022-06-07 14:59:17 +0800

    docs(iq): 5、处理render方法

--- docs/interview-questions/6、Vue2/vue2-study/src/init.js
+++ docs/interview-questions/6、Vue2/vue2-study/src/init.js
@@ -6,5 +6,25 @@ export function initMixin(Vue) {
     vm.$options = options
 
     initState(vm)
+
+    if(vm.$Options.el) {
+      vm.$mount(vm.$options.el)
+    }
+    
+  }
+
+  Vue.prototype.$mount = function(el) {
+    const vm = this
+    const options = vm.$options
+    el = document.querySelector(el)
+
+    if(!options.render) {
+      let template = options.template
+      if(!template && el) {
+        template = el.outerHTML
+        let render = compileToFunction(template)
+        options.render = render
+      }
+    }
   }
 }
