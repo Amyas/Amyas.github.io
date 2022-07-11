1d493eccbed9302d8e537b84b07d1d46bf909b78
wangjianpeng <wangjianpeng@weimiao.cn>
2022-07-11 12:49:00 +0800

    docs(iq): vue3
    搭建vue3

---
+++ docs/interview-questions/8、Vue3/vue/vue3-study/scripts/dev.js
@@ -0,0 +1,43 @@
+// minimist可以解析命令行参数
+const args = require("minimist")(process.argv.slice(2));
+const path = require("path");
+
+const target = args._[0];
+const format = args.f;
+const entry = path.resolve(__dirname, `../packages/${target}/src/index.ts`);
+const packageName = require(path.resolve(
+  __dirname,
+  `../packages/${target}/package.json`
+)).buildOptions?.name;
+
+// iife 自执行函数 global
+// cjs  commonjs规范
+// esm  es6Module
+const outputFormat = format.startsWith("global")
+  ? "iife"
+  : format === "cjs"
+  ? "cjs"
+  : "esm";
+const outfile = path.resolve(
+  __dirname,
+  `../packages/${target}/dist/${target}.${format}.index.js`
+);
+
+const { build } = require("esbuild");
+
+build({
+  entryPoints: [entry],
+  outfile,
+  bundle: true,
+  sourcemap: true,
+  format: outputFormat,
+  globalName: packageName,
+  platform: format === "cjs" ? "node" : "browser",
+  watch: {
+    onRebuild(error) {
+      if (!error) console.log("rebuild~~~");
+    },
+  },
+}).then(() => {
+  console.log("watching~~~");
+});
