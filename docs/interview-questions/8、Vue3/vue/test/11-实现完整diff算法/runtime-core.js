5759cd7f19381af813501a1f4b8e14be0f0c6c12
wangjianpeng <wangjianpeng@weimiao.cn>
2022-07-28 15:00:34 +0800

    docs(iq): vue3
    11-2

--- docs/interview-questions/8、Vue3/vue/test/11-实现完整diff算法/runtime-core.js
+++ docs/interview-questions/8、Vue3/vue/test/11-实现完整diff算法/runtime-core.js
@@ -82,6 +82,11 @@ function createRenderer(options) {
   } = options;
 
   function patch(n1, n2, container) {
+    if (n1 && !isSameVnode(n1, n2)) {
+      unmount(n1);
+      n1 = null;
+    }
+
     const { type, shapeFlag } = n2;
 
     switch (type) {
@@ -105,6 +110,8 @@ function createRenderer(options) {
   function processElement(n1, n2, container) {
     if (n1 === null) {
       mountElement(n2, container);
+    } else {
+      patchElement(n1, n2);
     }
   }
 
@@ -112,6 +119,10 @@ function createRenderer(options) {
     const { type, props, children, shapeFlag } = vnode;
 
     const el = (vnode.el = hostCreateElement(type));
+
+    if (props) {
+      patchProps(null, props, el);
+    }
 
     if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
       hostSetElementText(el, children);
@@ -138,8 +149,161 @@ function createRenderer(options) {
     return children[i];
   }
 
+  function patchProps(oldProps, newProps, el) {
+    if (oldProps === null) oldProps = {};
+    if (newProps === null) newProps = {};
+
+    for (let key in newProps) {
+      hostPatchProp(el, key, oldProps[key], newProps[key]);
+    }
+
+    for (let key in oldProps) {
+      if (newProps[key] === null) {
+        hostPatchProp(el, key, oldProps[key], null);
+      }
+    }
+  }
+
+  function patchElement(n1, n2) {
+    const el = (n2.el = n1.el);
+    const oldProps = n1.props;
+    const newProps = n2.props;
+
+    patchProps(oldProps, newProps, el);
+
+    patchChildren(n1, n2, el);
+  }
+
+  function patchChildren(oldVnode, newVnode, el) {
+    const oldChildren = oldVnode.children;
+    const newChildren = newVnode.children;
+
+    const oldShapeFlag = oldVnode.shapeFlag;
+    const newShapeFlag = newVnode.shapeFlag;
+
+    if (newShapeFlag & ShapeFlags.TEXT_CHILDREN) {
+      if (oldShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+        unmountChildren(oldChildren);
+      }
+
+      if (oldChildren !== newChildren) {
+        hostSetElementText(el, newChildren);
+      }
+    } else {
+      if (oldShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+        if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+          patchKeyedChildren(oldChildren, newChildren, el);
+        } else {
+          unmountChildren(oldChildren);
+        }
+      } else {
+        if (oldShapeFlag & ShapeFlags.TEXT_CHILDREN) {
+          hostSetElementText(el, "");
+        }
+
+        if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+          mountChildren(newChildren, el);
+        }
+      }
+    }
+  }
+
+  function patchKeyedChildren(oldChildren, newChildren, el) {
+    let index = 0;
+    let oldLastIndex = oldChildren.length - 1;
+    let newLastIndex = newChildren.length - 1;
+
+    while (index <= oldLastIndex && index <= newLastIndex) {
+      const oldVnode = oldChildren[index];
+      const newVnode = newChildren[index];
+      if (isSameVnode(oldVnode, newVnode)) {
+        patch(oldVnode, newVnode, el);
+      } else {
+        break;
+      }
+      i++;
+    }
+
+    while (index <= oldLastIndex && index <= newLastIndex) {
+      const oldVnode = oldChildren[oldLastIndex];
+      const newVnode = newChildren[newLastIndex];
+      if (isSameVnode(oldVnode, newVnode)) {
+        patch(oldVnode, newVnode, el);
+      } else {
+        break;
+      }
+      oldLastIndex--;
+      newLastIndex--;
+    }
+
+    if (index > oldLastIndex) {
+      if (index <= newLastIndex) {
+        while (index <= newLastIndex) {
+          const nextPos = newLastIndex + 1;
+          const anchor =
+            newChildren.length <= nextPos ? null : newChildren[nextPos].el;
+          patch(null, newChildren[index], el, anchor);
+          index++;
+        }
+      }
+    } else if (index > newLastIndex) {
+      if (index <= oldLastIndex) {
+        while (index <= oldLastIndex) {
+          unmount(oldChildren[index]);
+          index++;
+        }
+      }
+    }
+
+    let s1 = index;
+    let s2 = index;
+
+    let toBePatched = newLastIndex - s2 + 1;
+    const keyToNewIndexMap = new Map();
+    for (let i = s2; i <= oldLastIndex; i++) {
+      keyToNewIndexMap.set(newChildren[i].key, i);
+    }
+
+    for (let i = s1; i <= oldLastIndex; i++) {
+      const oldVnode = oldChildren[i];
+      const newIndex = keyToNewIndexMap.get(oldVnode.key);
+
+      if (!newIndex) {
+        unmount(oldVnode);
+      } else {
+        patch(oldVnode, newChildren[newIndex], el);
+      }
+    }
+
+    for (let i = toBePatched - 1; i >= 0; i--) {
+      const currentIndex = s2 + 1;
+      const child = newChildren[currentIndex];
+      const anchor =
+        currentIndex + 1 < newChildren.length
+          ? newChildren[currentIndex + 1].el
+          : null;
+
+      if (child.el === null) {
+        patch(null, child, el.anchor);
+      } else {
+        hostInsert(child.el, el, anchor);
+      }
+    }
+  }
+
+  function unmountChildren(children) {
+    children.forEach((child) => {
+      unmount(child);
+    });
+  }
+
+  function unmount(vnode) {
+    hostRemove(vnode.el);
+  }
+
   function render(vnode, container) {
     if (vnode === null) {
+      unmount(container._vnode);
     } else {
       patch(container._vnode || null, vnode, container);
     }
