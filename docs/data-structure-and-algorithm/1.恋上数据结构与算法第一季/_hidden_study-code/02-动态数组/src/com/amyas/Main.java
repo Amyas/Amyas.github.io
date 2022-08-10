c374e28cbb650682ed9ee7e9405edfa344625b2f
wangjianpeng <wangjianpeng@weimiao.cn>
2022-08-10 11:47:02 +0800

    docs(al): 接口设计

---
+++ docs/data-structure-and-algorithm/1.恋上数据结构与算法第一季/_hidden_study-code/02-动态数组/src/com/amyas/Main.java
@@ -0,0 +1,12 @@
+package com.amyas;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int array[] = new int[] { 11, 22, 33 };
+
+		ArrayList list = new ArrayList();
+		list.get(0);
+	}
+
+}
