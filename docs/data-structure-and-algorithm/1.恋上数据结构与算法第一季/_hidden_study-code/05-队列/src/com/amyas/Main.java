e74cc329a45a633344a74768f4632b1bbe078a78
wangjianpeng <wangjianpeng@weimiao.cn>
2022-08-25 14:55:31 +0800

    docs(al): 循环双端队列

--- docs/data-structure-and-algorithm/1.恋上数据结构与算法第一季/_hidden_study-code/05-队列/src/com/amyas/Main.java
+++ docs/data-structure-and-algorithm/1.恋上数据结构与算法第一季/_hidden_study-code/05-队列/src/com/amyas/Main.java
@@ -1,5 +1,6 @@
 package com.amyas;
 
+import com.amyas.cirle.CircleDeque;
 import com.amyas.cirle.CircleQueue;
 
 public class Main {
@@ -43,7 +44,36 @@ public class Main {
     System.out.println(queue);
   }
 
+  static void test3() {
+    CircleDeque<Integer> queue = new CircleDeque<>();
+    // 头5 4 3 2 1 100 101 102 103 104 105 106 8 7 6 尾
+
+    // 头 8 7 6 5 4 3 2 1 100 101 102 103 104 105 106 107 108 109 null null 10 9 尾
+    for (int i = 0; i < 10; i++) {
+      queue.enQueueFront(i + 1);
+      queue.enQueueRear(i + 100);
+    }
+    System.out.println(queue);
+
+    // 头 null 7 6 5 4 3 2 1 100 101 102 103 104 105 106 null null null null null
+    // null null 尾
+    for (int i = 0; i < 3; i++) {
+      queue.deQueueFront();
+      queue.deQueueRear();
+    }
+
+    // 头 11 7 6 5 4 3 2 1 100 101 102 103 104 105 106 null null null null null null
+    // 12 尾
+    queue.enQueueFront(11);
+    queue.enQueueFront(12);
+    System.out.println(queue);
+    while (!queue.isEmpty()) {
+      System.out.println(queue.deQueueFront());
+    }
+  }
+
   public static void main(String[] args) {
-    test2();
+    // test2();
+    test3();
   }
 }
