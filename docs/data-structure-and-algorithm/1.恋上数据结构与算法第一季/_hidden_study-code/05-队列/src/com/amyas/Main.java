fedf4549b5277b4e51484d59b5eabba5f5f633db
wangjianpeng <wangjianpeng@weimiao.cn>
2022-08-25 12:43:44 +0800

    docs(al): 循环队列 - 实现与扩容

--- docs/data-structure-and-algorithm/1.恋上数据结构与算法第一季/_hidden_study-code/05-队列/src/com/amyas/Main.java
+++ docs/data-structure-and-algorithm/1.恋上数据结构与算法第一季/_hidden_study-code/05-队列/src/com/amyas/Main.java
@@ -1,7 +1,9 @@
 package com.amyas;
 
+import com.amyas.cirle.CircleQueue;
+
 public class Main {
-  public static void main(String[] args) {
+  static void test1() {
     // Queue<Integer> queue = new Queue<>();
     // queue.enQueue(1);
     // queue.enQueue(2);
@@ -15,10 +17,33 @@ public class Main {
     queue.enQueueRear(4);
     System.out.println(queue);
 
-    // 2134
-
     while (!queue.isEmpty()) {
       System.out.println(queue.deQueueFront());
     }
+  }
+
+  static void test2() {
+    CircleQueue<Integer> queue = new CircleQueue<Integer>();
+    // 0 1 2 3 4 5 6 7 8 9
+    for (int i = 0; i < 10; i++) {
+      queue.enQueue(i);
+    }
+    // null null null null null 5 6 7 8 9
+    for (int i = 0; i < 5; i++) {
+      queue.deQueue();
+    }
+    // 15 16 17 18 19 5 6 7 8 9
+    for (int i = 15; i < 20; i++) {
+      queue.enQueue(i);
+    }
+    System.out.println(queue);
+    while (!queue.isEmpty()) {
+      System.out.println(queue.deQueue());
+    }
+    System.out.println(queue);
+  }
+
+  public static void main(String[] args) {
+    test2();
   }
 }
