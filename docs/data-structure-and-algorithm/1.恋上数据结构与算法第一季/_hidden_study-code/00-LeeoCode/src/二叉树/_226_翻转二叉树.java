af45a3ea7d7cbd2b97e6da51b38281e9593e80d9
wangjianpeng <wangjianpeng@weimiao.cn>
2022-08-29 15:55:53 +0800

    docs(al): 反转二叉树

---
+++ docs/data-structure-and-algorithm/1.恋上数据结构与算法第一季/_hidden_study-code/00-LeeoCode/src/二叉树/_226_翻转二叉树.java
@@ -0,0 +1,48 @@
+package 二叉树;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * https://leetcode.cn/problems/invert-binary-tree/
+ */
+public class _226_翻转二叉树 {
+  public TreeNode invertTree(TreeNode root) {
+    if (root == null) {
+      return root;
+    }
+
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+
+    while(!queue.isEmpty()) {
+      TreeNode node = queue.poll();
+      TreeNode tmp = node.left;
+      node.left = node.right;
+      node.right = tmp;
+      
+      if(node.left != null) {
+        queue.offer(node.left);
+      }
+      if(node.right != null) {
+        queue.offer(node.right);
+      }
+    }
+
+    return root;
+  }
+  public TreeNode invertTree1(TreeNode root) {
+    if (root == null) {
+      return root;
+    }
+
+    TreeNode tmp = root.left;
+    root.left = root.right;
+    root.right = tmp;
+
+    invertTree(root.left);
+    invertTree(root.right);
+
+    return root;
+  }
+}
