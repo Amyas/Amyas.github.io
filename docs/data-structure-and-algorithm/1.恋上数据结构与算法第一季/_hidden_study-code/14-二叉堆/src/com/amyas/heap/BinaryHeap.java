a08402b21e3e61dc7d648f1c6932c75da4b6b6c6
wangjianpeng <wangjianpeng@weimiao.cn>
2022-08-30 18:21:41 +0800

    docs(al): 14.二叉堆
    2.堆

---
+++ docs/data-structure-and-algorithm/1.恋上数据结构与算法第一季/_hidden_study-code/14-二叉堆/src/com/amyas/heap/BinaryHeap.java
@@ -0,0 +1,40 @@
+package com.amyas.heap;
+
+public class BinaryHeap<E> implements Heap<E> {
+
+	@Override
+	public int size() {
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return false;
+	}
+
+	@Override
+	public void clear() {
+
+	}
+
+	@Override
+	public void add(E element) {
+
+	}
+
+	@Override
+	public E get() {
+		return null;
+	}
+
+	@Override
+	public E remove() {
+		return null;
+	}
+
+	@Override
+	public E replace(E element) {
+		return null;
+	}
+
+}
