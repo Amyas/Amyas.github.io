6f03fd3f4f50cf45515839e33ec18c6ec209631b
wangjianpeng <wangjianpeng@weimiao.cn>
2022-08-31 14:30:05 +0800

    docs(al): 选择

--- docs/data-structure-and-algorithm/2.恋上数据结构与算法第二季/_hidden_study-code/01-排序/src/com/amyas/Main.java
+++ docs/data-structure-and-algorithm/2.恋上数据结构与算法第二季/_hidden_study-code/01-排序/src/com/amyas/Main.java
@@ -1,28 +1,48 @@
 package com.amyas;
 
+import com.amyas.tools.Asserts;
 import com.amyas.tools.Integers;
-import com.amyas.tools.Times;
 
 public class Main {
   public static void main(String[] args) {
     // Integer[] array1 = Integers.random(10000, 1, 100000);
     // Integer[] array1 = Integers.ascOrder(1, 10000);
-    Integer[] array1 = Integers.tailAscOrder(1, 10000, 2000);
-    Integer[] array2 = Integers.copy(array1);
-    Integer[] array3 = Integers.copy(array1);
+    // Integer[] array1 = Integers.tailAscOrder(1, 10000, 2000);
+    // Integer[] array2 = Integers.copy(array1);
+    // Integer[] array3 = Integers.copy(array1);
     // Integers.println(array1);
 
-    Times.test("冒泡排序bubbleSort1", () -> {
-      bubbleSort1(array1);
-    });
+    // Times.test("冒泡排序bubbleSort1", () -> {
+    // bubbleSort1(array1);
+    // });
 
-    Times.test("冒泡排序bubbleSort2", () -> {
-      bubbleSort2(array2);
-    });
+    // Times.test("冒泡排序bubbleSort2", () -> {
+    // bubbleSort2(array2);
+    // });
 
-    Times.test("冒泡排序bubbleSort3", () -> {
-      bubbleSort3(array3);
-    });
+    // Times.test("冒泡排序bubbleSort3", () -> {
+    // bubbleSort3(array3);
+    // });
+
+    Integer[] array = Integers.random(10, 1, 100);
+    Integers.println(array);
+    selectorSort(array);
+    Integers.println(array);
+    Asserts.test(Integers.isAscOrder(array));
+  }
+
+  static void selectorSort(Integer[] array) {
+    for (int end = array.length - 1; end > 0; end--) {
+      int maxIndex = 0;
+      for (int begin = 1; begin <= end; begin++) {
+        if (array[maxIndex] <= array[begin]) {
+          maxIndex = begin;
+        }
+      }
+      int tmp = array[maxIndex];
+      array[maxIndex] = array[end];
+      array[end] = tmp;
+    }
   }
 
   static void bubbleSort1(Integer[] array) {
