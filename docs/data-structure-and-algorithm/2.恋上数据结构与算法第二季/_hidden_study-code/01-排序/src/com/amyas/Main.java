7626feab9877cd0a3afbbf10f396d45dc3495958
wangjianpeng <wangjianpeng@weimiao.cn>
2022-08-31 15:29:53 +0800

    docs(al): 增加范型和优化

--- docs/data-structure-and-algorithm/2.恋上数据结构与算法第二季/_hidden_study-code/01-排序/src/com/amyas/Main.java
+++ docs/data-structure-and-algorithm/2.恋上数据结构与算法第二季/_hidden_study-code/01-排序/src/com/amyas/Main.java
@@ -7,35 +7,12 @@ import com.amyas.sort.BubbleSort2;
 import com.amyas.sort.BubbleSort3;
 import com.amyas.sort.SelectionSort;
 import com.amyas.sort.Sort;
+import com.amyas.tools.Asserts;
 import com.amyas.tools.Integers;
 
+@SuppressWarnings({ "rawtypes", "unchecked" })
 public class Main {
   public static void main(String[] args) {
-    // Integer[] array1 = Integers.random(10000, 1, 100000);
-    // Integer[] array1 = Integers.ascOrder(1, 10000);
-    // Integer[] array1 = Integers.tailAscOrder(1, 10000, 2000);
-    // Integer[] array2 = Integers.copy(array1);
-    // Integer[] array3 = Integers.copy(array1);
-    // Integers.println(array1);
-
-    // Times.test("冒泡排序bubbleSort1", () -> {
-    // bubbleSort1(array1);
-    // });
-
-    // Times.test("冒泡排序bubbleSort2", () -> {
-    // bubbleSort2(array2);
-    // });
-
-    // Times.test("冒泡排序bubbleSort3", () -> {
-    // bubbleSort3(array3);
-    // });
-
-    // Integer[] array = Integers.random(10, 1, 100);
-    // Integers.println(array);
-    // selectorSort(array);
-    // Integers.println(array);
-    // Asserts.test(Integers.isAscOrder(array));
-
     Integer[] array = Integers.random(1000, 1, 20000);
     testSorts(
         array,
@@ -45,72 +22,13 @@ public class Main {
 
   static void testSorts(Integer[] array, Sort... sorts) {
     for (Sort sort : sorts) {
-      sort.sort(Integers.copy(array));
+      Integer[] newArray = Integers.copy(array);
+      sort.sort(newArray);
+      Asserts.test(Integers.isAscOrder(newArray));
     }
     Arrays.sort(sorts);
     for (Sort sort : sorts) {
       System.out.println(sort);
-    }
-  }
-
-  static void selectorSort(Integer[] array) {
-    for (int end = array.length - 1; end > 0; end--) {
-      int maxIndex = 0;
-      for (int begin = 1; begin <= end; begin++) {
-        if (array[maxIndex] <= array[begin]) {
-          maxIndex = begin;
-        }
-      }
-      int tmp = array[maxIndex];
-      array[maxIndex] = array[end];
-      array[end] = tmp;
-    }
-  }
-
-  static void bubbleSort1(Integer[] array) {
-    for (int end = array.length - 1; end > 0; end--) {
-      for (int begin = 1; begin <= end; begin++) {
-        if (array[begin] < array[begin - 1]) {
-          int tmp = array[begin];
-          array[begin] = array[begin - 1];
-          array[begin - 1] = tmp;
-        }
-      }
-    }
-  }
-
-  static void bubbleSort2(Integer[] array) {
-    for (int end = array.length - 1; end > 0; end--) {
-      boolean sorted = true;
-      for (int begin = 1; begin <= end; begin++) {
-        if (array[begin] < array[begin - 1]) {
-          int tmp = array[begin];
-          array[begin] = array[begin - 1];
-          array[begin - 1] = tmp;
-          sorted = false;
-        }
-      }
-
-      if (sorted) {
-        break;
-      }
-    }
-  }
-
-  static void bubbleSort3(Integer[] array) {
-    for (int end = array.length - 1; end > 0; end--) {
-      // sortedIndex 的初始值在数组完全有序的时候有用
-      int sortedIndex = 1;
-      for (int begin = 1; begin <= end; begin++) {
-        if (array[begin] < array[begin - 1]) {
-          int tmp = array[begin];
-          array[begin] = array[begin - 1];
-          array[begin - 1] = tmp;
-          sortedIndex = begin;
-        }
-      }
-
-      end = sortedIndex;
     }
   }
 }
--- docs/data-structure-and-algorithm/2.恋上数据结构与算法第二季/_hidden_study-code/01-排序/src/com/amyas/sort/BubbleSort2.java
+++ docs/data-structure-and-algorithm/2.恋上数据结构与算法第二季/_hidden_study-code/01-排序/src/com/amyas/sort/BubbleSort2.java
@@ -1,6 +1,6 @@
 package com.amyas.sort;
 
-public class BubbleSort2 extends Sort {
+public class BubbleSort2<E extends Comparable<E>> extends Sort<E> {
   @Override
   protected void sort() {
     for (int end = array.length - 1; end > 0; end--) {
@@ -8,6 +8,7 @@ public class BubbleSort2 extends Sort {
       for (int begin = 1; begin <= end; begin++) {
         if (cmp(begin, begin - 1) < 0) {
           swap(begin, begin - 1);
+          sorted = false;
         }
       }
       if (sorted) {
