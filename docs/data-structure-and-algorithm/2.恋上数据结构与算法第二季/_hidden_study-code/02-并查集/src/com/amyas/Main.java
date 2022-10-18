847878b0b5fa6641533e2e2897ddc7e12705d1e8
wangjianpeng <wangjianpeng@weimiao.cn>
2022-10-18 09:50:02 +0800

    docs(al): 并查集

--- docs/data-structure-and-algorithm/2.恋上数据结构与算法第二季/_hidden_study-code/02-并查集/src/com/amyas/Main.java
+++ docs/data-structure-and-algorithm/2.恋上数据结构与算法第二季/_hidden_study-code/02-并查集/src/com/amyas/Main.java
@@ -1,5 +1,27 @@
 package com.amyas;
 
-public class Main {
+import com.amyas.union.UnionFind;
 
+public class Main {
+  public static void main(String[] args) {
+    UnionFind uf = new UnionFind(12);
+    uf.union(0, 1);
+    uf.union(0, 3);
+    uf.union(0, 4);
+    uf.union(2, 3);
+    uf.union(2, 5);
+
+    uf.union(6, 7);
+
+    uf.union(8, 10);
+    uf.union(9, 10);
+    uf.union(9, 11);
+
+    System.out.println(uf.isSame(0, 6));
+    System.out.println(uf.isSame(0, 5));
+
+    System.out.println(uf.isSame(2, 7));
+    uf.union(4, 6);
+    System.out.println(uf.isSame(2, 7));
+  }
 }
--- docs/data-structure-and-algorithm/2.恋上数据结构与算法第二季/_hidden_study-code/02-并查集/src/com/amyas/union/UnionFind.java
+++ docs/data-structure-and-algorithm/2.恋上数据结构与算法第二季/_hidden_study-code/02-并查集/src/com/amyas/union/UnionFind.java
@@ -1,5 +1,58 @@
 package com.amyas.union;
 
 public class UnionFind {
-	
+  private int[] parents;
+
+  public UnionFind(int capacity) {
+    if (capacity < 0) {
+      throw new IllegalArgumentException("capacity must be >= 1");
+    }
+
+    parents = new int[capacity];
+    for (int i = 0; i < parents.length; i++) {
+      parents[i] = i;
+    }
+  }
+
+  /**
+   * 查找v所属的集合（根节点）
+   * 
+   * @param v
+   * @return
+   */
+  public int find(int v) {
+    rangeCheck(v);
+    return parents[v];
+  }
+
+  /**
+   * 检查v1、v2是否属于同一个集合
+   * 
+   * @param v1
+   * @param v2
+   * @return
+   */
+  public boolean isSame(int v1, int v2) {
+    return find(v1) == find(v2);
+  }
+
+  public void union(int v1, int v2) {
+    int p1 = find(v1);
+    int p2 = find(v2);
+    if (p1 == p2) {
+      return;
+    }
+
+    for (int i = 0; i < parents.length; i++) {
+      if (parents[i] == p1) {
+        parents[i] = p2;
+      }
+    }
+  }
+
+  private void rangeCheck(int v) {
+    if (v < 0 || v >= parents.length) {
+      throw new IllegalArgumentException("v is out of bounds");
+    }
+  }
 }
